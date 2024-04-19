576da61d1a3a2216d17e5e496fd4fa172e25f347
Mburu Rionge <victormburu3@gmail.com>
2024-04-19 07:33:24 -0700

    updates

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,27 +1,21 @@
-# Setup New Ubuntu server with nginx
-
-exec { 'update system':
-    command => '/usr/bin/apt-get update',
-}
-
+#using puppet to automate
+#
 package { 'nginx':
     ensure  => 'installed',
-    require => Exec['update system'],
+}
+
+file_line { 'install':
+	ensure => 'present',
+	path => '/etc/nginx/sites-enabled/default',
+	after => 'listen 80 default_server;',
+	line => 'rewrte ^/redirect_me https://www.github.com/mbururionge permanent;',
 }
 
 file { '/var/www/html/index.html':
     content => 'Hello World!',
 }
 
-file { '/etc/nginx/sites-available/default':
-    ensure  => present,
-    content => template('module_name/default_config.erb'),
-    notify  => Service['nginx'],
-}
-
 service { 'nginx':
-    ensure  => running,
-    enable  => true,
-    require => Package['nginx'],
+	ensure => running,
+	require => Package['nginx'],
 }
-File['/etc/nginx/sites-available/default'] ~> Service['nginx']
