5318e758268e0633596d9491ea0f1c82f92f05dd
MburuRionge <victormburu3@gmail.com>
2024-08-15 01:12:50 +0000

    fixing an internal server error in apache

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,6 +1,7 @@
 # Fixed bad "phpp" extensions to "php" in "wp-settings.php".
 
 exec{ 'fix-wordpress':
-	command	=> 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
-	path	=> '/usr/local/bin/:/bin/'
+	command	=> 'sed -i \'s/phpp/php/g\' /var/www/html/wp-settings.php',
+	path	=> '/usr/local/bin/:/bin/',
+	unless	=> 'grep -q "phpp" /var/www/html/wp-settings.php'
 }
